3a4fe7dfc2963ee45a689464144f8f12a14fcfa8
Vlad Barsiienko <vlad_bars@ukr.net>
2025-02-06 22:43:28 +0200

    [Config + Refactor]: - removed redundant dependencies; - updated local Collector port after appSettings merge.

--- ObserviX.Collector/Program.cs
+++ ObserviX.Collector/Program.cs
@@ -1,14 +1,8 @@
 using ObserviX.API.ServiceDefaults;
-using ObserviX.Collector.Features;
 using ObserviX.Collector.Features.Visitors;
 using ObserviX.Shared;
-using ObserviX.Shared.Exceptions;
 using ObserviX.Shared.Extensions.ApiResponseWrapper;
 using ObserviX.Shared.Extensions.Caching;
-using ObserviX.Shared.Extensions.Configuration;
-using ObserviX.Shared.Extensions.Logging;
-using ObserviX.Shared.Extensions.Scalar;
-using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddSharedServices();
